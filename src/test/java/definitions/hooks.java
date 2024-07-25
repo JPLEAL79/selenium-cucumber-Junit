786e857f6647ff16f00ad7dc5e596687787f4665
JPLEAL79 <jpleal79@gmail.com>
2024-07-24 21:00:13 -0400

    remotewebdriver

--- src/test/java/definitions/hooks.java
+++ src/test/java/definitions/hooks.java
@@ -5,7 +5,10 @@ import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class hooks {
@@ -14,11 +17,11 @@ public class hooks {
     public static WebDriver driver;
 
     @Before
-    public static void setUp(){
+    public static void setUp() throws MalformedURLException {
         ChromeOptions opts = new ChromeOptions();
         opts.addArguments("--disable-notifications");
         opts.addArguments("--start-maximized");
-        driver = new ChromeDriver(opts);
+        driver = new RemoteWebDriver(new URL("http://3.141.41.156:4444"),opts);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
